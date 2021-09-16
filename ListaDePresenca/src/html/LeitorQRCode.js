9e92396c67c509024b0a217585557f5567e2d445
bonafe <bonafe@opapagaiorinoceronte.net>
2021-09-16 00:01:35 -0300

    atualiações

--- ListaDePresenca/src/html/LeitorQRCode.js
+++ ListaDePresenca/src/html/LeitorQRCode.js
@@ -1,19 +1,27 @@
-export class LeitorQRCode{
-    constructor(){
+//USANDO A BIBLIOTECA
+//https://github.com/mebjas/html5-qrcode
 
+export class LeitorQRCode{
+    
+    constructor(){
+        this.cameras = document.querySelector("#cameras");
         this.qrCodeLidos = document.querySelector("#qrCodeLidos");        
         this.ultimoQRCodeLido = undefined;
         this.listaQRCode = [];
+
+        this.cameras.addEventListener("change", ()=>{
+          this.iniciarLeitor(this.cameras.value);
+        });
 
         document.querySelector("#btnEnviarLista").addEventListener("click", () =>{
           console.log ("Enviando lista de QRCodes lidos");
           console.dir(this.listaQRCode);
         });
 
-        this.selecionarCamera();
+        this.povoarListaCameras();
     }
 
-    selecionarCamera(){
+    povoarListaCameras(){
         // This method will trigger user permissions
         Html5Qrcode.getCameras().then(devices => {
           /**
@@ -21,20 +29,34 @@ export class LeitorQRCode{
            * { id: "id", label: "label" }
            */
           if (devices && devices.length) {
-            this.cameraId = devices[0].id;
-            this.iniciarLeitor();
+
+            //Povoa o select com as câmeras disponíveis
+            for (let iDevice in devices){
+              
+              let device = devices[iDevice];
+              let camera = document.createElement("option");
+              camera.value = device.id;
+              camera.innerHTML = device.label;
+              if (iDevice == (devices.length-1)){
+                camera.selected = true;
+              }
+              this.cameras.appendChild(camera);
+            };
+
+            //Coloca o leitor para funcionar com a última câmera da lista
+            this.iniciarLeitor(devices[devices.length-1].id);
           }
         }).catch(err => {
           // handle err
         });
     }
 
-    iniciarLeitor(){
+    iniciarLeitor(cameraId){
         // Create instance of the object. The only argument is the "id" of HTML element created above.
         const html5QrCode = new Html5Qrcode("reader");
 
         html5QrCode.start(
-          this.cameraId,     // retreived in the previous step.
+          cameraId,     // retreived in the previous step.
           {
             fps: 10,    // sets the framerate to 10 frame per second
             qrbox: 250  // sets only 250 X 250 region of viewfinder to
@@ -46,11 +68,11 @@ export class LeitorQRCode{
                 this.ultimoQRCodeLido = qrCodeMessage;
                 this.adicionarItem(this.ultimoQRCodeLido);
             }
-            console.log(`QR Code detected: ${qrCodeMessage}`);
+            //console.log(`QR Code detected: ${qrCodeMessage}`);
           },
           errorMessage => {
             // parse error, ideally ignore it. For example:
-            console.log(`QR Code no longer in front of camera.`);
+            //console.log(`QR Code no longer in front of camera.`);
           })
         .catch(err => {
           // Start failed, handle it. For example,
@@ -66,6 +88,6 @@ export class LeitorQRCode{
         li.innerText = `${agora.toISOString()} --- ${valorQRCode}`;        
         this.qrCodeLidos.insertBefore(li, this.qrCodeLidos.firstChild);
 
-        this.listaQRCode.appendChild ({data:agora, conteudo:valorQRCode});
+        this.listaQRCode.push ({data:agora, conteudo:valorQRCode});
     }
 }
