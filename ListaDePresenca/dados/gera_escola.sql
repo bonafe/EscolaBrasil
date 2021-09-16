fc63053072bc51e7b15e52eb46ca1210b87aaf07
vpmammana <52040920+vpmammana@users.noreply.github.com>
2021-09-16 02:26:09 -0300

    Add files via upload

--- ListaDePresenca/dados/gera_escola.sql
+++ ListaDePresenca/dados/gera_escola.sql
@@ -5,25 +5,27 @@ USE escolax;
 
 create table turmas (id_chave_turma int not null auto_increment, nome_turma varchar(100), primary key (id_chave_turma), unique(nome_turma));
 
-create table professores (id_chave_professor int not null auto_increment, nome_professor varchar(200), primary key (id_chave_professor));
+create table tipos_vinculos(id_chave_tipo_vinculo int not null auto_increment, nome_tipo_vinculo varchar(100), primary key (id_chave_tipo_vinculo));
 
-create table turmas_professores (id_chave_turma_professor int not null auto_increment, nome_turma_professor varchar(100), id_turma int, id_professor int, primary key (id_chave_turma_professor));
+create table registrados(id_chave_registrado int not null auto_increment, nome_registrado varchar(200), primary key (id_chave_registrado));
 
-create table estudantes(id_chave_estudante int not null auto_increment, nome_estudante varchar(200), id_turma int, primary key (id_chave_estudante));
+create table turmas_registrados (id_chave_turma_registrado int not null auto_increment, nome_turma_registrado varchar(100), id_turma int, id_registrado int, id_tipo_vinculo int, primary key (id_chave_turma_registrado));
 
-create table presencas(id_chave_presenca int not null auto_increment, nome_presenca varchar(100), id_estudante int, time_stamp timestamp, primary key (id_chave_presenca));
+create table presencas(id_chave_presenca int not null auto_increment, nome_presenca varchar(100), id_registrado int, time_stamp timestamp, primary key (id_chave_presenca));
 
-ALTER TABLE estudantes ADD CONSTRAINT FK_estudante_turma FOREIGN KEY (id_turma) REFERENCES turmas(id_chave_turma);
-ALTER TABLE presencas ADD CONSTRAINT FK_estudante_presenca FOREIGN KEY (id_estudante) REFERENCES estudantes(id_chave_estudante);
-ALTER TABLE turmas_professores ADD CONSTRAINT FK_turma_professor FOREIGN KEY (id_turma) REFERENCES turmas(id_chave_turma);
-ALTER TABLE turmas_professores ADD CONSTRAINT FK_professor_turma FOREIGN KEY (id_professor) REFERENCES professores(id_chave_professor);
+ALTER TABLE presencas ADD CONSTRAINT FK_registrado_presenca FOREIGN KEY (id_registrado) REFERENCES registrados(id_chave_registrado);
+ALTER TABLE turmas_registrados ADD CONSTRAINT FK_turma_registrado FOREIGN KEY (id_turma) REFERENCES turmas(id_chave_turma);
+ALTER TABLE turmas_registrados ADD CONSTRAINT FK_registrado_turma FOREIGN KEY (id_registrado) REFERENCES registrados(id_chave_registrado);
+ALTER TABLE turmas_registrados ADD CONSTRAINT FK_registrado_vinculo FOREIGN KEY (id_tipo_vinculo) REFERENCES tipos_vinculos(id_chave_tipo_vinculo);
 
-insert into professores(nome_professor) values ( "Joana");
-insert into professores(nome_professor) values ( "Tatiane");
-insert into professores(nome_professor) values ( "Alfredo");
-insert into professores(nome_professor) values ( "Maria");
-insert into professores(nome_professor) values ( "Pedro");
 
+insert into tipos_vinculos(nome_tipo_vinculo) values ("nao_aplica");
+insert into tipos_vinculos(nome_tipo_vinculo) values ("estudante");
+insert into tipos_vinculos(nome_tipo_vinculo) values ("diretor(a)");
+insert into tipos_vinculos(nome_tipo_vinculo) values ("professor(a)");
+
+
+insert into turmas (nome_turma) values ("nao_aplica");
 insert into turmas (nome_turma) values ("Primeiro Ano A");
 insert into turmas (nome_turma) values ("Primeiro Ano B");
 
@@ -33,20 +35,63 @@ insert into turmas (nome_turma) values ("Segundo Ano B");
 insert into turmas (nome_turma) values ("Terceiro Ano A");
 insert into turmas (nome_turma) values ("Terceiro Ano B");
 
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Primeiro Ano A"), "Isabela");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Primeiro Ano A"), "Victoria");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Primeiro Ano A"), "Jose");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Primeiro Ano A"), "Mario");
 
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Primeiro Ano B"), "Roberto");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Primeiro Ano B"), "Paulo");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Primeiro Ano B"), "Fabio");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Primeiro Ano B"), "Carolina");
+insert into registrados(nome_registrado) values ( "nao_aplica");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="nao_aplica"),(select id_chave_turma from turmas where nome_turma="nao_aplica"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="nao_aplica"));
 
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Segundo Ano B"), "Roberta");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Segundo Ano B"), "Paula");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Segundo Ano B"), "Fabia");
-insert into estudantes (id_turma, nome_estudante) values ((select id_chave_turma from turmas where nome_turma="Segundo Ano B"), "Tania");
+insert into registrados(nome_registrado) values ( "Joana");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Joana"),(select id_chave_turma from turmas where nome_turma="Primeiro Ano A"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados(nome_registrado) values ( "Tatiane");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Tatiane"),(select id_chave_turma from turmas where nome_turma="Primeiro Ano A"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados(nome_registrado) values ( "Alfredo");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Alfredo"),(select id_chave_turma from turmas where nome_turma="Primeiro Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados(nome_registrado) values ( "Maria");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Maria"),(select id_chave_turma from turmas where nome_turma="Primeiro Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados(nome_registrado) values ( "Pedro");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Pedro"),(select id_chave_turma from turmas where nome_turma="Primeiro Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados (nome_registrado) values ( "Isabela");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Isabela"),(select id_chave_turma from turmas where nome_turma="Segundo Ano A"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados (nome_registrado) values ( "Victoria");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Victoria"),(select id_chave_turma from turmas where nome_turma="Segundo Ano A"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+
+
+insert into registrados (nome_registrado) values ( "Jose");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Jose"),(select id_chave_turma from turmas where nome_turma="Segundo Ano A"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados (nome_registrado) values ( "Mario");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Mario"),(select id_chave_turma from turmas where nome_turma="Segundo Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados (nome_registrado) values ( "Roberto");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Roberto"),(select id_chave_turma from turmas where nome_turma="Segundo Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados (nome_registrado) values ( "Paulo");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Paulo"),(select id_chave_turma from turmas where nome_turma="Segundo Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados (nome_registrado) values ( "Fabio");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Fabio"),(select id_chave_turma from turmas where nome_turma="Segundo Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+insert into registrados (nome_registrado) values ( "Carolina");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Carolina"),(select id_chave_turma from turmas where nome_turma="Terceiro Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="estudante"));
+
+
+insert into registrados (nome_registrado) values ("Roberta");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Roberta"),(select id_chave_turma from turmas where nome_turma="Terceiro Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="professor(a)"));
+
+insert into registrados (nome_registrado) values ("Paula");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Paula"),(select id_chave_turma from turmas where nome_turma="Primeiro Ano B"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="professor(a)"));
+
+insert into registrados (nome_registrado) values ("Fabia");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Fabia"),(select id_chave_turma from turmas where nome_turma="Primeiro Ano A"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="professor(a)"));
+
+insert into registrados (nome_registrado) values ("Tania");
+insert into turmas_registrados (id_registrado, id_turma, id_tipo_vinculo) values ((select id_chave_registrado from registrados where nome_registrado="Tania"),(select id_chave_turma from turmas where nome_turma="Segundo Ano A"),(select id_chave_tipo_vinculo from tipos_vinculos where nome_tipo_vinculo="professor(a)"));
 
 
 
