8f3409d32655e7480929be81a27e9708aa570212
vpmammana <52040920+vpmammana@users.noreply.github.com>
2021-09-16 02:27:59 -0300

    Add files via upload

--- ListaDePresenca/php_backend/registra_presenca.php
+++ ListaDePresenca/php_backend/registra_presenca.php
@@ -1,8 +1,12 @@
 <?php
-
+// por Victor
 if(isset($_GET["nome"])){
   $nome = $_GET["nome"];
 } else {$nome="nao forneceu nome";}
+
+if(isset($_GET["id"])){
+  $id = $_GET["id"];
+} else {$id="nao forneceu id";}
 
 if(isset($_GET["dia"])){
   $dia = $_GET["dia"];
@@ -11,8 +15,8 @@ if(isset($_GET["dia"])){
 
 
 
-$username="victor";
-$pass="ops";
+$username="vitor";
+$pass="SenhaMyVi2021";
 $database="escolax";
 $agora=date('Y-m-d');
 
@@ -34,12 +38,12 @@ table, td {
 
 $conn= new mysqli("localhost", $username, $pass, $database);
 
-$sql="insert into presencas (id_estudante) values ((select id_chave_estudante from estudantes where nome_estudante='".$nome."'))";
+$sql="insert into presencas(id_registrado) values (".$id.")";
 
 if ($conn->query($sql)===true){ echo "Presenca de ".$nome." foi registrada.";} else {echo "<br> Deu problema com o sql: ".$sql." erro:".$conn->error;}
 
 
-$sql="select nome_estudante, time_stamp from estudantes, presencas where id_estudante=id_chave_estudante and time_stamp like '".$dia."%'";
+$sql="select nome_registrado, time_stamp from registrados, presencas where id_registrado=id_chave_registrado and time_stamp like '".$dia."%'";
 
 
 $result=$conn->query("$sql");
@@ -48,9 +52,9 @@ if ($result->num_rows>0) {
 echo "<table>";
 
     while($row=$result->fetch_assoc()){
-	$nome_estudante=$row["nome_estudante"];
+	$nome_registrado=$row["nome_registrado"];
 	$time_stamp=$row["time_stamp"];
-	echo "<tr><td>".$nome_estudante."</td><td>".$time_stamp."</td></tr>";
+	echo "<tr><td>".$nome_registrado."</td><td>".$time_stamp."</td></tr>";
 	}
 echo "</table>";
 }
