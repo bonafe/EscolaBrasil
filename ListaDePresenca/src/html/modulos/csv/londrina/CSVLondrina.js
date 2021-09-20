43cc80706b5b01a882b74498e1e25c8568d35e9a
bonafe <bonafe@opapagaiorinoceronte.net>
2021-09-20 19:21:06 -0300

    data corrigida

--- ListaDePresenca/src/html/modulos/csv/londrina/CSVLondrina.js
+++ ListaDePresenca/src/html/modulos/csv/londrina/CSVLondrina.js
@@ -7,19 +7,18 @@ export class CSVLondrina{
         let turma = prompt("Digite o nome da turma:","sem turma");
         
         let linhas = conteudo.split("\n");
-
-        let inicioDados = 14;
-
+        
         let numeroAtual = null;
 
         let terminou = false;
 
         window.listaAlunos = [];
 
-        for (let iLinha = inicioDados; (iLinha < linhas.length) && !terminou; iLinha++){
+        for (let iLinha = 0; (iLinha < linhas.length) && !terminou; iLinha++){
+
             let campos = linhas[iLinha].split(";");
             let numeroLinha = parseInt(campos[0]);
-
+            
             //Se é um número inteiro guarda como número do aluno atual
             if (Number.isInteger(numeroLinha)){
                 numeroAtual = numeroLinha;
@@ -27,7 +26,7 @@ export class CSVLondrina{
 
             try{
                 //Se tem conteudo e não tem @ é porque é o nome do aluno
-                if ((campos[1].indexOf("@") == -1) && (campos[1].length > 0)){
+                if ((campos[1].indexOf("@") == -1) && (campos[1].length > 0) && numeroAtual){
                     console.log (`${numeroAtual} --- ${campos[1]}`);
                     window.listaAlunos.push({turma:turma, numero:numeroAtual, nome:campos[1]});
                 }
